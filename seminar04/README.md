4a16a7204b71db60dc6d541a9f890420eaae503d
lex0819 <lexmolnar@gmail.com>
2023-07-07 13:25:49 +0300

    hw04

--- seminar04/README.md
+++ seminar04/README.md
@@ -1,6 +1,6 @@
 # Урок 4. SQL – работа с несколькими таблицами
 
-Условие:
+См. скрипты с решениями в файле [hw04.sql](./hw04.sql) более подробно.
 
 ## Табличка: https://docs.google.com/document/d/1kEwTJhGJeyfNFdr8t7O4ioiqiFthoy4cQZ2CKaDuwY8/edit?usp=sharing
 
@@ -8,12 +8,51 @@
 
 ## 1.Вывести всех котиков по магазинам по id (условие соединения shops.id = cats.shops_id)
 
+```sql
+SELECT cats.name, shops.shopname
+FROM cats
+JOIN shops
+ON cats.shops_id = shops.id;
+```
+
 ## 2.Вывести магазин, в котором продается кот “Мурзик”
 
 (попробуйте выполнить 2 способами)
 
+```sql
+SELECT s.shopname, c.name
+FROM shops s, cats c
+WHERE c.shops_id = s.id AND c.name = "Murzik";
+
+SELECT s.shopname, c.name
+FROM shops s
+JOIN (SELECT * FROM cats
+	WHERE name = "Murzik") AS c
+ON s.id = c.shops_id;
+```
+
 ## 3.Вывести магазины, в которых НЕ продаются коты “Мурзик” и “Zuza”
+
+```sql
+SELECT s.shopname, c.name
+FROM shops s
+JOIN (SELECT * FROM cats
+	WHERE name NOT IN ("Murzik", "Zuza") ) AS c
+ON s.id = c.shops_id;
+```
 
 ## Табличка (после слов “Последнее задание, таблица:”)
 
 ## 4.Вывести название и цену для всех анализов, которые продавались 5 февраля 2020 и всю следующую неделю.
+
+```sql
+SELECT a.an_name AS "Название", a.an_price AS "ЦЕНА", o.ord_datetime AS "Дата продажи"
+FROM analysis a
+JOIN
+	(SELECT * FROM Orders
+	WHERE Orders.ord_datetime BETWEEN '2020-02-05 00:00:00'
+		AND DATE_ADD('2020-02-05 00:00:00', INTERVAL 1 WEEK)
+	) AS o
+ON a.an_id = o.ord_an
+ORDER BY o.ord_datetime;
+```
--- seminar04/hw04.sql
+++ seminar04/hw04.sql
@@ -3,6 +3,8 @@ CREATE DATABASE `lesson4`;
 
 USE lesson4;
 
+-- Магазин котиков --
+
 DROP TABLE IF EXISTS `shops`;
 CREATE TABLE `shops` (
 	`id` INT,
@@ -54,12 +56,151 @@ ON cats.shops_id = shops.id;
 
 
 -- Вывести магазин, в котором продается кот “Мурзик”
-SELECT id, name, shops_id
-FROM cats
-WHERE name = "Murzik";
 
-SELECT sh.*, c.*
-FROM shops sh
+SELECT s.shopname, c.name
+FROM shops s, cats c
+WHERE c.shops_id = s.id AND c.name = "Murzik";
+
+SELECT s.shopname, c.name
+FROM shops s
 JOIN (SELECT * FROM cats
 	WHERE name = "Murzik") AS c
-ON sh.id = c.shops_id;
+ON s.id = c.shops_id;
+
+
+-- Вывести магазины, в которых НЕ продаются коты “Мурзик” и “Zuza”
+
+SELECT s.shopname, c.name
+FROM shops s
+JOIN (SELECT * FROM cats
+	WHERE name != "Murzik" AND name != "Zuza") AS c
+ON s.id = c.shops_id;
+
+
+SELECT s.shopname, c.name
+FROM shops s
+JOIN (SELECT * FROM cats
+	WHERE name NOT IN ("Murzik", "Zuza") ) AS c
+ON s.id = c.shops_id;
+
+
+
+-- Медицинские анализы --
+
+DROP TABLE IF EXISTS GroupsAn;
+
+CREATE TABLE GroupsAn
+(
+	gr_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	gr_name varchar(50),
+	gr_temp FLOAT(5,1)	
+);
+
+INSERT INTO groupsan (gr_name, gr_temp)
+VALUES 
+	('Анализы крови', -12.2),
+	('Общие анализы', -20.0),
+	('ПЦР-диагностика', -20.5);
+
+SELECT * FROM groupsan;
+
+
+
+DROP TABLE IF EXISTS Analysis;
+
+CREATE TABLE Analysis
+(
+	an_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	an_name varchar(50),
+	an_cost INT,
+	an_price INT,
+	an_group INT,
+    FOREIGN KEY (an_group) REFERENCES GroupsAn (gr_id)
+	ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO analysis (an_name, an_cost, an_price, an_group)
+VALUES 
+	('Общий анализ крови', 30, 50, 1),
+	('Биохимия крови', 150, 210, 1),
+	('Анализ крови на глюкозу', 110, 130, 1),
+	('Общий анализ кала', 35, 50, 2),
+	('Общий анализ мочи', 25, 40, 2),
+	('Тест на COVID-19', 160, 210, 3);
+    
+    
+SELECT * FROM analysis;
+
+
+DROP TABLE IF EXISTS Orders;
+CREATE TABLE Orders
+(
+	ord_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	ord_datetime DATETIME,	-- 'YYYY-MM-DD hh:mm:ss'
+	ord_an INT,
+	FOREIGN KEY (ord_an) REFERENCES Analysis (an_id)
+	ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO Orders (ord_datetime, ord_an)
+VALUES 
+	('2020-02-04 07:15:25', 1),
+	('2020-02-04 07:20:50', 2),
+	('2020-02-04 07:30:04', 1),
+	('2020-02-04 07:40:57', 1),
+	('2020-02-05 07:05:14', 1),
+	('2020-02-05 07:15:15', 3),
+	('2020-02-05 07:30:49', 3),
+	('2020-02-06 07:10:10', 2),
+	('2020-02-06 07:20:38', 2),
+	('2020-02-07 07:05:09', 1),
+	('2020-02-07 07:10:54', 1),
+	('2020-02-07 07:15:25', 1),
+	('2020-02-08 07:05:44', 1),
+	('2020-02-08 07:10:39', 2),
+	('2020-02-08 07:20:36', 1),
+	('2020-02-08 07:25:26', 3),
+	('2020-02-09 07:05:06', 1),
+	('2020-02-09 07:10:34', 1),
+	('2020-02-09 07:20:19', 2),
+	('2020-02-10 07:05:55', 3),
+	('2020-02-10 07:15:08', 3),
+	('2020-02-10 07:25:07', 1),
+	('2020-02-11 07:05:33', 1),
+	('2020-02-11 07:10:32', 2),
+	('2020-02-11 07:20:17', 3),
+	('2020-02-12 07:05:36', 1),
+	('2020-02-12 07:10:54', 2),
+	('2020-02-12 07:20:19', 3),
+	('2020-02-12 07:35:38', 1);
+
+SELECT * FROM Orders;
+
+
+-- Анализы по группам --
+
+SELECT * FROM Analysis a
+JOIN GroupsAn g
+ON a.an_group = g.gr_id
+ORDER BY g.gr_id;
+
+
+-- Даты продажи с 5 февраля 2020 и всю следующую неделю.
+
+SELECT * FROM Orders
+WHERE Orders.ord_datetime BETWEEN '2020-02-05 00:00:00' 
+	AND DATE_ADD('2020-02-05 00:00:00', INTERVAL 1 WEEK)
+ORDER BY Orders.ord_datetime;
+
+
+-- Вывести название и цену для всех анализов, которые продавались 5 февраля 2020 и всю следующую неделю.
+
+SELECT a.an_name AS "Название", a.an_price AS "ЦЕНА", o.ord_datetime AS "Дата продажи"
+FROM analysis a
+JOIN 
+	(SELECT * FROM Orders
+	WHERE Orders.ord_datetime BETWEEN '2020-02-05 00:00:00' 
+		AND DATE_ADD('2020-02-05 00:00:00', INTERVAL 1 WEEK)
+	) AS o
+ON a.an_id = o.ord_an
+ORDER BY o.ord_datetime;
